172f07b14138e75944747332af649921216c43d8
halilibrahimakca <halilibrahimakcaa@gmail.com>
2023-08-06 11:29:08 +0300

    Revised Topbar.jsx component 🔄

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,11 +1,40 @@
 import "./topbar.css";
-
+import { Search, Person, Chat, Notifications } from "@mui/icons-material";
 export default function Topbar() {
   return (
     <div className="topbarContainer">
-      <div className="topbarLeft"></div>
-      <div className="topbarCenter"></div>
-      <div className="topbarRight"></div>
+      <div className="topbarLeft">
+        <span className="logo">React Social</span>
+      </div>
+      <div className="topbarCenter">
+        <div className="searchbar">
+          <Search />
+          <input
+            placeholder="Search for friend, post or video"
+            className="searchInput"
+          />
+        </div>
+      </div>
+      <div className="topbarRight">
+        <div className="topbarLinks">
+          <span className="topbarLink">Homepage</span>
+          <span className="topbarLink">Timeline</span>
+        </div>
+        <div className="topbarIcons">
+          <div className="topbarIconItem">
+            <Person />
+            <span className="topbarIconBadge">1</span>
+          </div>
+          <div className="topbarIconItem">
+            <Chat />
+            <span className="topbarIconBadge">1</span>
+          </div>
+          <div className="topbarIconItem">
+            <Notifications />
+            <span className="topbarIconBadge">1</span>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
