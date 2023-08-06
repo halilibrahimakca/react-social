3a8caa7f89e32a73bbf3067916a13541f44400e8
halilibrahimakca <halilibrahimakcaa@gmail.com>
2023-08-06 03:01:06 +0300

    Initial project setup 🌱

---
+++ src/components/topbar/Topbar.jsx
@@ -0,0 +1,11 @@
+import "./topbar.css";
+
+export default function Topbar() {
+  return (
+    <div className="topbarContainer">
+      <div className="topbarLeft"></div>
+      <div className="topbarCenter"></div>
+      <div className="topbarRight"></div>
+    </div>
+  );
+}
